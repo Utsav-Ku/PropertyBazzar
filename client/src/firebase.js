0ebf2c4b811cabbbcc6167496309640be1aba080
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-02-09 17:11:37 +0530

    complete image upload functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
+ // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "propertybazzar-b355a.firebaseapp.com",
-  projectId: "propertybazzar-b355a",
-  storageBucket: "propertybazzar-b355a.firebasestorage.app",
-  messagingSenderId: "226415555956",
-  appId: "1:226415555956:web:3d6f6a725cb5e5af80a821",
-  measurementId: "G-83FQER7EEP"
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "homevista1-45a85.firebaseapp.com",
+  projectId: "homevista1-45a85",
+  storageBucket: "homevista1-45a85.appspot.com",
+  messagingSenderId: "1043662371982",
+  appId: "1:1043662371982:web:d40746bfddc994dfcb9ddf"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
