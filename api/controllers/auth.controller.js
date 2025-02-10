21492ee7d7fc990d19c0cfeb76c0fd6740e20603
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-02-10 18:44:57 +0530

    add sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -37,9 +37,9 @@ export const google = async(req, res, next) => {
             const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
             const { password: pass, ...rest } = user._doc;
             res
-                .cookie('access_token',token,{httpOnly: true})
-                .status(200)
-                .json(rest);
+            .cookie('access_token',token,{httpOnly: true})
+            .status(200)
+            .json(rest);
         } else{
             const generatedPassword = Math.random().toString(36).slice(-8) +  Math.random().toString(36).slice(-8);
             const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
@@ -48,10 +48,19 @@ export const google = async(req, res, next) => {
             const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
             const { password: pass, ...rest } = newUser._doc;
             res
-                .cookie('access_token',token,{httpOnly: true})
-                .status(200)
-                .json(rest);
+            .cookie('access_token',token,{httpOnly: true})
+            .status(200)
+            .json(rest);
         }
+    } catch(error){
+        next(error);
+    }
+}
+
+export const signOut = async(req, res, next) => {
+    try{
+        res.clearCookie('access_token');
+        res.status(200).json('User has been logged out!');
     } catch(error){
         next(error);
     }
