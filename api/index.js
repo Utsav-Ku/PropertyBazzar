e048ede38d68dcfe8753cde73a42ecd75a81694d
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-02-21 21:02:59 +0530

    add vercel json file

--- api/index.js
+++ api/index.js
@@ -5,7 +5,16 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+// const cors = require('cors');
+import cors from 'cors';
+import path from 'path';
+
+
 dotenv.config();
+
+const app = express();
+app.use(cors());
+
 
 mongoose.connect(process.env.MONGO).then(() =>{
         console.log('Connected to MongoDB!');
@@ -14,7 +23,6 @@ mongoose.connect(process.env.MONGO).then(() =>{
     }
 );
 
-const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,6 +35,17 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+
+
+const __dirname=path.resolve()
+console.log(__dirname);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'client', 'dist', 'index.html'));
+});
 
 app.use((err, req,res,next) => {
     const statusCode = err.statusCode || 500;
